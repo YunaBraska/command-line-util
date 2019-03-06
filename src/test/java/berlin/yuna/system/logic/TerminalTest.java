f2c761694097a76ca139023234ab04c5461440d8
yuna90 <io@yuna.berlin>
2019-03-06 07:28:12 +0100

    fixed tests

--- src/test/java/berlin/yuna/system/logic/TerminalTest.java
+++ src/test/java/berlin/yuna/system/logic/TerminalTest.java
@@ -63,7 +63,7 @@ public class TerminalTest {
     @Test
     public void clearLogs_shouldClearInfoAndErrorOutput() {
         terminal.execute("echo \"Howdy\"");
-        terminal.breakOnError(false).execute("invalidCommand");
+        terminal.timeoutMs(10).breakOnError(false).execute("invalidCommand");
         assertThat(terminal.consoleInfo().length(), is(6));
         assertThat(terminal.consoleError().length(), is(38));
 
@@ -74,7 +74,7 @@ public class TerminalTest {
 
     @Test
     public void execute_withWrongCommandAndNoBreakOnError_shouldPrintConsoleErrorOutput() {
-        terminal.breakOnError(false).execute("invalidCommand");
+        terminal.timeoutMs(10).breakOnError(false).execute("invalidCommand");
         assertThat(terminal.consoleError().toString(), equalTo("sh: invalidCommand: command not found" + LINE_SEPARATOR));
         assertThat(terminal.consoleInfo().length(), is(0));
     }
